35e66be203d4aec9d9e2240561dc20f4556df698
Jacob Larson <hockeybobber@comcast.net>
2024-11-20 21:17:13 -0600

    added the default template for header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
+function Header() {
+
+  return (
+    <div>
+      <header className='App-header'>
+        <h1 className='App-title'>Feedback!</h1>
+        <h4>Don't forget it!</h4>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
