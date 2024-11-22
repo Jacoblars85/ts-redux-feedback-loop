9b830920d8bb3cc828f02a8f20faae83973a47e7
Jacob Larson <hockeybobber@comcast.net>
2024-11-21 18:00:24 -0600

    imported all the compontents and added routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,50 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
-import Header from '../Header/Header';
+import React from "react";
+import axios from "axios";
+import "./App.css";
+import Header from "../Header/Header";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Feeling from "../Feeling/Feeling";
+import Understanding from "../Understanding/Understanding";
+import Support from "../Support/Support";
+import Comments from "../Comments/Comments";
+import Review from "../Review/Review";
+import ThankYou from "../ThankYou/ThankYou";
+import Admin from "../Admin/Admin";
 
 function App() {
-
   return (
-    <div className='App'>
-<Header />
+    <div className="App">
+      <Header />
+      
+      <Router>
+        <Route exact path="/">
+          <Feeling />
+        </Route>
+
+        <Route exact path="/understanding">
+          <Understanding />
+        </Route>
+
+        <Route exact path="/support">
+          <Support />
+        </Route>
+
+        <Route exact path="/comments">
+          <Comments />
+        </Route>
+
+        <Route exact path="/review">
+          <Review />
+        </Route>
+
+        <Route exact path="/thankyou">
+          <ThankYou />
+        </Route>
+
+        <Route exact path="/admin">
+          <Admin />
+        </Route>
+      </Router>
     </div>
   );
 }
