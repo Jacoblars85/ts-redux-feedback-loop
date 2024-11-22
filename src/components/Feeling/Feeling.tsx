6c0bfe3b9e4fc268f3de73e2f5921c3f8e6e4865
Jacob Larson <hockeybobber@comcast.net>
2024-11-21 18:12:43 -0600

    added a form with headers

--- src/components/Feeling/Feeling.tsx
+++ src/components/Feeling/Feeling.tsx
@@ -5,7 +5,24 @@ function Feeling() {
 
   return (
     <div>
+  <h1>How are you feeling today?</h1>
 
+
+<p>Feeling?</p>
+
+<form onSubmit={submitFeelings}>
+
+    <input
+        onChange={(e) => setFeelingInput(e.target.value)}
+        value={feelingInput}
+        type='text'
+        placeholder='1-5' />
+
+
+    <button>Next</button>
+
+
+</form>
     </div>
   );
 }
