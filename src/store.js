3e0eb4db65d4a62efa71628d0ef5f52185c12692
Jacob Larson <hockeybobber@comcast.net>
2024-11-21 18:56:03 -0600

    added feeling reducer

--- src/store.js
+++ src/store.js
@@ -2,10 +2,20 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 
 
+const feeling = (state = "", action) => {
+    if (action.type === 'SET_FEELING') {
+        return action.payload
+    }
+    if (action.type === 'CLEAR_REDUCERS') {
+        return ""
+    }
+    return state;
+}
+
 
 const store = createStore(
     combineReducers({
-
+        feeling,
     }),
     applyMiddleware(logger),
 );
