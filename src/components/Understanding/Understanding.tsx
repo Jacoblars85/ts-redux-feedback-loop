e925e62c3d9c43abc6a142443361a5d28fbbf433
Jacob Larson <hockeybobber@comcast.net>
2024-11-21 18:58:41 -0600

    added form

--- src/components/Understanding/Understanding.tsx
+++ src/components/Understanding/Understanding.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 function Understanding() {
-
   return (
     <div>
+      <h1>How well are you understanding the content?</h1>
 
+      <p>Understanding?</p>
+
+      <form onSubmit={submitUnderstanding}>
+        <input
+          onChange={(e) => setUnderstandingInput(e.target.value)}
+          value={understandingInput}
+          type="text"
+          placeholder="1-5"
+        />
+
+        <button>Next</button>
+      </form>
     </div>
   );
 }
