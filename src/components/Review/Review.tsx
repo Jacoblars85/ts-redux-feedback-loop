df2352df2fab269b3722d46c83e970f0e386bbd2
Jacob Larson <hockeybobber@comcast.net>
2024-11-22 20:37:08 -0600

    added post route and useselectors

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
 import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom';
 
 function Review() {
+
+    const feeling = useSelector((store) => store.feeling)
+    const understanding = useSelector((store) => store.understanding)
+    const support = useSelector((store) => store.support)
+    const comments = useSelector((store) => store.comments)
+
+    const dispatch = useDispatch()
+
+    const history = useHistory()
+
+    const postFeedback = () => {
+        axios({
+            method: 'POST',
+            url: '/feedback',
+            data: {
+                feeling: feeling,
+                understanding: understanding,
+                support: support,
+                comments: comments
+            }
+        }).then((response) => {
+
+        }).catch((err) => {
+            console.log(err);
+        });
+
+        dispatch({
+            type: 'CLEAR_REDUCERS'
+        })
+        history.push("/thankyou")
+    }
+
 
   return (
     <div>
