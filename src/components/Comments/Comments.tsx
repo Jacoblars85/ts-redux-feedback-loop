80ffb2d39a29c900a085b50b104a903fe1e6c38b
Jacob Larson <hockeybobber@comcast.net>
2024-11-22 20:41:19 -0600

    formatted doc

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -1,46 +1,42 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux'
-import { useHistory } from 'react-router-dom';
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 function Comments() {
+  const [commentsInput, setCommentsInput] = useState("");
 
-    const [commentsInput, setCommentsInput] = useState('');
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const history = useHistory();
 
-    const history = useHistory()
+  const submitComments = (e) => {
+    e.preventDefault();
 
-    const submitComments = (e) => {
-        e.preventDefault()
+    dispatch({
+      type: "SET_COMMENTS",
+      payload: commentsInput,
+    });
 
-        dispatch({
-            type: 'SET_COMMENTS',
-            payload: commentsInput
-        })
-
-        history.push("/review")
-
-    }
+    history.push("/review");
+  };
 
   return (
     <div>
-       <h1>Any comments you want to leave?</h1>
+      <h1>Any comments you want to leave?</h1>
 
-<p>Comments</p>
+      <p>Comments</p>
 
-<form onSubmit={submitComments}>
+      <form onSubmit={submitComments}>
+        <input
+          data-testid="input"
+          onChange={(e) => setCommentsInput(e.target.value)}
+          value={commentsInput}
+          type="text"
+          placeholder="text"
+        />
 
-    <input
-        data-testid="input"
-        onChange={(e) => setCommentsInput(e.target.value)}
-        value={commentsInput}
-        type='text'
-        placeholder='text' />
-
-    <button data-testid="next" >Next</button>
-
-</form>
+        <button data-testid="next">Next</button>
+      </form>
     </div>
   );
 }
