f69ed149cd47f76d87b1ae2c9b9c2320ac52dabf
Jacob Larson <hockeybobber@comcast.net>
2024-11-22 20:34:56 -0600

    added the p tags with all reducers

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -5,7 +5,18 @@ function Review() {
 
   return (
     <div>
+  <h1>Review Your Feedback</h1>
 
+<div className='reviewSizing'>
+
+<p>feelings: {feeling}</p>
+<p>understanding: {understanding}</p>
+<p>support: {support}</p>
+<p>Comments: {comments}</p>
+</div>
+<div className='reviewButton'>
+<button onClick={postFeedback} className='submitButton'>Submit</button>
+</div>
     </div>
   );
 }
