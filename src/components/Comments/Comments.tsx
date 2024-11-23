d539d82af34e880c8cdb06c6f3e07e1624fea370
Jacob Larson <hockeybobber@comcast.net>
2024-11-22 20:26:13 -0600

    added form

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -6,7 +6,22 @@ function Comments() {
 
   return (
     <div>
-     
+       <h1>Any comments you want to leave?</h1>
+
+<p>Comments</p>
+
+<form onSubmit={submitComments}>
+
+    <input
+        data-testid="input"
+        onChange={(e) => setCommentsInput(e.target.value)}
+        value={commentsInput}
+        type='text'
+        placeholder='text' />
+
+    <button data-testid="next" >Next</button>
+
+</form>
     </div>
   );
 }
