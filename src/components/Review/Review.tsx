7a4cdc4a670cff81597fb35c07245c9bd4b84fdb
Jacob Larson <hockeybobber@comcast.net>
2024-11-22 20:41:23 -0600

    formatted doc

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -1,56 +1,55 @@
-import React from 'react';
-import axios from 'axios';
-import { useSelector, useDispatch } from 'react-redux'
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 function Review() {
+  const feeling = useSelector((store) => store.feeling);
+  const understanding = useSelector((store) => store.understanding);
+  const support = useSelector((store) => store.support);
+  const comments = useSelector((store) => store.comments);
 
-    const feeling = useSelector((store) => store.feeling)
-    const understanding = useSelector((store) => store.understanding)
-    const support = useSelector((store) => store.support)
-    const comments = useSelector((store) => store.comments)
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const history = useHistory();
 
-    const history = useHistory()
+  const postFeedback = () => {
+    axios({
+      method: "POST",
+      url: "/feedback",
+      data: {
+        feeling: feeling,
+        understanding: understanding,
+        support: support,
+        comments: comments,
+      },
+    })
+      .then((response) => {})
+      .catch((err) => {
+        console.log(err);
+      });
 
-    const postFeedback = () => {
-        axios({
-            method: 'POST',
-            url: '/feedback',
-            data: {
-                feeling: feeling,
-                understanding: understanding,
-                support: support,
-                comments: comments
-            }
-        }).then((response) => {
-
-        }).catch((err) => {
-            console.log(err);
-        });
-
-        dispatch({
-            type: 'CLEAR_REDUCERS'
-        })
-        history.push("/thankyou")
-    }
-
+    dispatch({
+      type: "CLEAR_REDUCERS",
+    });
+    history.push("/thankyou");
+  };
 
   return (
     <div>
-  <h1>Review Your Feedback</h1>
+      <h1>Review Your Feedback</h1>
 
-<div className='reviewSizing'>
-
-<p>feelings: {feeling}</p>
-<p>understanding: {understanding}</p>
-<p>support: {support}</p>
-<p>Comments: {comments}</p>
-</div>
-<div className='reviewButton'>
-<button onClick={postFeedback} className='submitButton'>Submit</button>
-</div>
+      <div className="reviewSizing">
+        <p>feelings: {feeling}</p>
+        <p>understanding: {understanding}</p>
+        <p>support: {support}</p>
+        <p>Comments: {comments}</p>
+      </div>
+      <div className="reviewButton">
+        <button onClick={postFeedback} className="submitButton">
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
