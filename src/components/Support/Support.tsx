0b5137b33e65bd29aa6780d577229473eebefefe
Jacob Larson <hockeybobber@comcast.net>
2024-11-22 20:20:34 -0600

    added form

--- src/components/Support/Support.tsx
+++ src/components/Support/Support.tsx
@@ -6,6 +6,21 @@ function Support() {
   return (
     <div>
 
+<h1>How well are you being supported?</h1>
+
+<p>Support?</p>
+
+<form onSubmit={submitSupport}>
+
+    <input
+        onChange={(e) => setSupportInput(e.target.value)}
+        value={supportInput}
+        type='text'
+        placeholder='1-5' />
+
+    <button>Next</button>
+
+</form>
     </div>
   );
 }
