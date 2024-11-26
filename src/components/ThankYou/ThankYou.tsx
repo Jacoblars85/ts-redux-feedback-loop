129e67dccd81f525953534daaba21a7e4d42f026
Jacob Larson <hockeybobber@comcast.net>
2024-11-25 20:25:26 -0600

    added thank you page

--- src/components/ThankYou/ThankYou.tsx
+++ src/components/ThankYou/ThankYou.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function ThankYou() {
 
   return (
     <div>
 
+<div className='thankBody'>
+<h1>Thank You!</h1>
+
+<Link to="/">
+    <button className='thankButton'>Leave New Feedback</button>
+</Link>
+</div>
     </div>
   );
 }
