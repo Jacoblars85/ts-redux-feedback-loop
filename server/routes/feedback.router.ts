40c0123b69281cc0bbbc17dc0b234f175b544fdf
Jacob Larson <hockeybobber@comcast.net>
2024-11-25 20:18:03 -0600

    added post route

--- server/routes/feedback.router.ts
+++ server/routes/feedback.router.ts
@@ -5,7 +5,22 @@ const pool = require('../modules/pool')
 
 // TODO: This route adds a new feedback entry
 router.post('/', (req, res) => {
+    const sqlQueryText = `
+     INSERT INTO "feedback"
+          ("feeling", "understanding", "support", "comments")
+          VALUES
+          ($1, $2, $3, $4);
+    `
+    const sqlValues = [req.body.feeling, req.body.understanding, req.body.support, req.body.comments];
 
+    pool.query(sqlQueryText, sqlValues)
+        .then((result) => {
+            res.sendStatus(201);
+            console.log('POST successful');
+        }).catch((err) => {
+            console.log('error posting things', err);
+            res.sendStatus(500);
+        })
 })
 
 
