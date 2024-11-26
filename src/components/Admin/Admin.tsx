f81a44207e326f56d50f010eb2d739b2c8dd5c91
Jacob Larson <hockeybobber@comcast.net>
2024-11-25 20:36:31 -0600

    added get route for all feedback and called it in useEffect

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux'
+import AdminDelete from '../AdminDelete/AdminDelete';
 
 function Admin() {
+    const dispatch = useDispatch()
+
+    const feedback = useSelector((store) => store.feedback)
+
+    useEffect(() => {
+        getFeedback();
+    }, []);
+
+    const getFeedback = () => {
+        axios({
+            method: 'GET',
+            url: '/api/feedback'
+        })
+            .then((response) => {
+                dispatch({
+                    type: 'SET_FEEDBACK',
+                    payload: response.data
+                })
+            })
+            .catch((error) => {
+                console.log('error on GET', error);
+            });
+    }
 
   return (
     <div>
