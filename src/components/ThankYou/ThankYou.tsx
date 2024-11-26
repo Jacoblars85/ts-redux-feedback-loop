27be6d83f3aea69b2d9e573caa1a4fcd688a1146
Jacob Larson <hockeybobber@comcast.net>
2024-11-25 20:26:20 -0600

    formatted doc

--- src/components/ThankYou/ThankYou.tsx
+++ src/components/ThankYou/ThankYou.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function ThankYou() {
-
   return (
     <div>
+      <div className="thankBody">
+        <h1>Thank You!</h1>
 
-<div className='thankBody'>
-<h1>Thank You!</h1>
-
-<Link to="/">
-    <button className='thankButton'>Leave New Feedback</button>
-</Link>
-</div>
+        <Link to="/">
+          <button className="thankButton">Leave New Feedback</button>
+        </Link>
+      </div>
     </div>
   );
 }
