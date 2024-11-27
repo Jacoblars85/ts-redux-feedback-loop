adb08895afae58d9f16387fdc0a4da4db181bca0
Jacob Larson <hockeybobber@comcast.net>
2024-11-26 21:52:25 -0600

    added the table body to the commponent

--- src/components/AdminDelete/AdminDelete.tsx
+++ src/components/AdminDelete/AdminDelete.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import axios from 'axios';
 
-function AdminDelete() {
+function AdminDelete({ feed, getFeedback }) {
 
   return (
     <div>
-
+<tr>
+            <td>{feed.feeling}</td>
+            <td>{feed.understanding}</td>
+            <td>{feed.support}</td>
+            <td>"{feed.comments}"</td>
+            <td>
+                <button onClick={deleteFeedback}>delete</button>
+            </td>
+        </tr>
     </div>
   );
 }
